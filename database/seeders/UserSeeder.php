448a58d8087cf9d70d65ee6e70b317692af17ad3
SweetzDreamz <cpaigiszero@gmail.com>
2026-03-22 01:52:31 +0700

    feat: add nip to user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::create([
+            'name' => 'Admin BPS',
+            'nip' => '123',
+            'email' => 'admin@bps.go.id',
+            'password' => bcrypt('admin123'),
+            'role' => 'supervisor',
+        ]);
+    }
+}
